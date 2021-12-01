277fe119b7ba6971a5bec32737e01f7f0b25f5cb
Mark Vader <mvader1@ford.com>
2021-12-01 09:52:49 -0700

    Updated my code

--- Sources/AOC2021/Day 1/2021-Day1.swift
+++ Sources/AOC2021/Day 1/2021-Day1.swift
@@ -1,31 +1,68 @@
 //
 //  Day1.swift
-//  test
 //
-//  Created by Dave DeLong on 11/25/21.
+//  Created by Dave DeLong on 11/28/21.
 //  Copyright © 2021 Dave DeLong. All rights reserved.
 //
 
-import Algorithms
+import AOCCore
 
 class Day1: Day {
+  
+    override func run() -> (String, String) {
+        return super.run()
+    }
 
     override func part1() -> String {
-        let increases = input.lines.integers
-            .adjacentPairs()
-            .count { $1 > $0 }
+        let array1 = input.integers
+        let offsetArray1 = [0] + array1
+        let zippedArray1 = zip(offsetArray1, array1)
+        var counter1 = 0
+        let offsetArrayDroppingLast1 = Array(zippedArray1.dropFirst())
+        for item in offsetArrayDroppingLast1 {
+            if item.1 - item.0 > 0 {
+                counter1 += 1
+            }
+        }
         
-        return "\(increases)"
+        return String(counter1)
     }
-
+    
     override func part2() -> String {
-        let increases = input.lines.integers
-            .windows(ofCount: 3)
-            .map(\.sum)
-            .adjacentPairs()
-            .count { $1 > $0 }
+        let array = input.integers
+        let offsetArray = [0] + array
+        let zippedArray = zip(offsetArray, array)
+        var counter = 0
+        let offsetArrayDroppingLast = Array(zippedArray.dropFirst())
+        let chunked1 = array.chunks(of: 3)
+        let pick1 = array.takeThree(startPickPointRange: 0)
+        let indices = array.enumerated().map{ $0.0 }
+        let stuff = indices.map{ array.takeThree(startPickPointRange: $0) }.compactMap{ $0 }
+        let sums = stuff.map { $0.sum }
+        let array1 = sums
+        let offsetArray1 = [0] + array1
+        let zippedArray1 = zip(offsetArray1, array1)
+        var counter1 = 0
+        let offsetArrayDroppingLast1 = Array(zippedArray1.dropFirst())
+        for item in offsetArrayDroppingLast1 {
+            if item.1 - item.0 > 0 {
+                counter1 += 1
+            }
+        }
         
-        return "\(increases)"
+        return String(counter1)
     }
+    
+}
 
+extension Array {
+    
+    func takeThree(startPickPointRange: Int) -> Array? {
+        let endPickPointRange = startPickPointRange + 2
+        guard endPickPointRange < count else { return nil }
+        let answer = Array(self[startPickPointRange...endPickPointRange])
+        
+        return answer
+    }
+    
 }
